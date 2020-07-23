9ab8a6c59ea57a749ab97c54ffe82e517eead7b0
chihirosakyo <c.sakyou@gmail.com>
2020-07-23 15:19:49 +0900

    add

---
+++ index.js
@@ -0,0 +1,14 @@
+let color = "赤";
+
+// 以下にswitch文を書いてください。
+switch(color) {
+    case "金":
+        console.log("ハワイ旅行券");
+        break;
+    case "赤":
+        console.log("国内温泉巡り旅行券");
+        break;
+    case "青":
+        console.log("商品券1万円");
+        break;
+}
