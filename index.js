77cf063bd26772f766d0c78688f9a090ac993407
chihirosakyo <c.sakyou@gmail.com>
2020-07-24 22:08:24 +0900

    add

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-let color = "赤";
+//  color = "赤";
 
 // 以下にswitch文を書いてください。
-switch(color) {
-    case "金":
-        console.log("ハワイ旅行券");
-        break;
-    case "赤":
-        console.log("国内温泉巡り旅行券");
-        break;
-    case "青":
-        console.log("商品券1万円");
-        break;
+// switch(color) {
+//     case "金":
+//         console.log("ハワイ旅行券");
+//         break;
+//     case "赤":
+//         console.log("国内温泉巡り旅行券");
+//         break;
+//     case "青":
+//         console.log("商品券1万円");
+//         break;
+// }
+
+for (i=1; i<10; i++){
+    console.log(`xの値は${i}`);
 }
